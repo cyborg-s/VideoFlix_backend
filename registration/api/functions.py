8ee80ea8dfa3f83f20853af595f89d307bc58fe1
cyborg-s <nysascha1@gmail.com>
2025-06-19 15:48:18 +0200

    email tamplate

--- registration/api/functions.py
+++ registration/api/functions.py
@@ -5,27 +5,35 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse
 from django.contrib.sites.shortcuts import get_current_site
+from email.mime.image import MIMEImage
 
 def send_activation_email(user, request):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
-    domain = get_current_site(request).domain
-    activation_link = f"http://{domain}" + reverse("activate", kwargs={"uidb64": uid, "token": token})
+    
+    # Frontend URL zur Login-Seite mit uid & token als Query-Parameter
+    frontend_domain = "http://localhost:4200"
+    activation_link = f"{frontend_domain}/login?uidb64={uid}&token={token}"
 
     subject = "Aktiviere dein VideoFlix-Konto"
     from_email = "noreply@videoflix.com"
     to = [user.email]
 
     context = {
-    "user": user,
-    "activation_link": activation_link,
-    "username_clean": user.username.split("_")[0],
-}
+        "user": user,
+        "activation_link": activation_link,
+        "username_clean": user.username.split("_")[0],
+    }
 
     text_content = f"Bitte klicke auf den folgenden Link zur Aktivierung:\n\n{activation_link}"
     html_content = render_to_string("emails/activation_email.html", context)
 
     msg = EmailMultiAlternatives(subject, text_content, from_email, to)
     msg.attach_alternative(html_content, "text/html")
+
+    with open('./registration/templates/emails/Capa_1.png', 'rb') as f:
+        logo = MIMEImage(f.read())
+        logo.add_header('Content-ID', '<logo_image>')  # cid im HTML verwenden
+        msg.attach(logo)
+
     msg.send()
- 
